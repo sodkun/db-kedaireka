d741ce3c3b9d815adbaf7c375fdd2100fa0c70e3
agistarakha <agistarakhasnc@gmail.com>
2022-09-15 00:31:01 +0700

    FPPP migration

---
+++ database/migrations/2022_09_14_171629_create_products_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("FPPP_number")->nullable();
+            $table->string("project_name")->nullable();
+            $table->string("applicator_name")->nullable();
+            $table->string("file_bom_alumunium")->nullable();
+            $table->string("file_bom_aksesoris")->nullable();
+            $table->string("file_wo_alumunium")->nullable();
+            $table->string("file_wo_kaca")->nullable();
+            $table->string("production_phase")->nullable();
+            $table->string("user_name")->nullable();
+            $table->string("production_time")->nullable();
+            $table->string("color")->nullable();
+            $table->string("glass_type")->nullable();
+            $table->text("note")->nullable();
+            $table->integer("quotation_id")->nullable();
+            $table->enum("status_order", [])->nullable();
+            $table->enum("glass", [])->nullable();
+            $table->enum("box_usage", [])->nullable();
+            $table->enum("sealant_usage", [])->nullable();
+            $table->enum("delivery_to_expedition", [])->nullable();
+            $table->date("retrieval_deadline")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
