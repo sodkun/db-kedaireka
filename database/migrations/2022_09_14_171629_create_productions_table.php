c6ff9aefe2578a6db3fbb765cd77555bb8a59e0b
pupunn <pupunn@students.unnes.ac.id>
2022-09-15 11:45:55 +0700

    update productions

--- database/migrations/2022_09_14_171629_create_productions_table.php
+++ database/migrations/2022_09_14_171629_create_productions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('productions', function (Blueprint $table) {
             $table->id();
             $table->string("FPPP_number")->nullable();
             $table->string("project_name")->nullable();
@@ -46,6 +46,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('productions');
     }
 };
