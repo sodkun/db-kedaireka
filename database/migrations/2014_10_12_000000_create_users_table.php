fcfcc291bed04f83b5441b04d10821d3b6cecd7b
ab-azmi <azmi1752@students.unnes.ac.id>
2022-10-01 08:36:51 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,12 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('gender');
+            $table->string('phone_number')->nullable();
+            $table->string('address')->nullable();
+            $table->integer('active');
+            $table->string('photo')->nullable();
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
