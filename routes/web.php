32becfc4b49e274d143b25412615ec832997caff
Ridwan Ridlo <murtiniibu110@gmail.com>
2022-10-03 12:00:00 +0700

    s

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("/login", [LoginController::class, "index"]);
+Route::get("/login", [LoginController::class, "index"])->name("login");
 
 Route::post("/login", [LoginController::class, "login"]);
 
